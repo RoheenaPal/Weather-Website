b032822deea6661e49813eedf7a511323567e901
Roheena Pal <palroheena@gmail.com>
2023-07-16 10:59:38 +0530

    fixed the code so that the local time is displayed

--- public/JS/main.js
+++ public/JS/main.js
@@ -19,13 +19,77 @@ const getInfo = async(event) => {
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];
-            console.log(arrData);
 
             city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
             temp_real_val.innerText = arrData[0].main.temp;
 
+            const getCurrentDay = () => {
+                let weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+                
+                let currentTime = new Date();
+                // var offset = currentTime.getTimezoneOffset();
+                // console.log(offset);
+
+                let days = weekday[currentTime.getUTCDay()];
+                let day = document.getElementById('day');
+    
+                day.innerText = days;
+            };
+    
+            getCurrentDay();
+    
+            const getCurrentTime = () => {
+                let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    
+                var now = new Date();
+                // console.log(now);
+                var month = months[now.getUTCMonth()];
+                var date = now.getUTCDate();
+                // console.log(date)
+                var time_modify = arrData[0].timezone;
+                // console.log(now.getUTCHours())
+                let hours = Math.floor(now.getUTCHours() + (time_modify / 3600));
+                // console.log(hours)
+                let mins = Math.floor(now.getUTCMinutes() + ((time_modify % 3600) / 60));
+    
+                let period = "AM"
+                if(hours < 0){
+                    hours = 24 + hours;
+                    // console.log(new Date().getUTCDay());
+                    let days = new Date().getUTCDay();
+                    let day = document.getElementById('day');
+                    date -= 1;
+                    if(days > 0){
+                        day.innerText = weekday[days - 1];
+                    }
+                    else{
+                        day.innerText = "Saturday";
+                    }
+                }
+                // console.log(hours)
+                if(hours > 11){
+                    period = "PM";
+                    if(hours > 12){
+                        hours -= 12;
+                    }
+                }
+                if(hours == 0){
+                    hours = 12;
+                }
+    
+                if(mins < 10){
+                    mins = "0" + mins;
+                }
+                let date_today = document.getElementById('todayDate')
+    
+                date_today.innerText = `${month} ${date} | ${hours}:${mins}${period}` 
+            };
+    
+            getCurrentTime();
+
+
             const tempMood = arrData[0].weather[0].main;
-            console.log(tempMood);
+            // console.log(tempMood);
 
             if(tempMood == "Clear"){
                 temp_status.innerHTML = 
@@ -41,7 +105,7 @@ const getInfo = async(event) => {
                     "<i class='fa fa-cloud' style='color: #f1f2f6; aria-hidden='true'></i>"
             }
             dataHide.classList.remove('data_hide');
-            console.log(data);
+            // console.log(data);
         }
         catch{
             city_name.innerText = `Please enter the city name properly`;
